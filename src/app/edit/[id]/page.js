ac1133968531857b88b9da642a0cf2b1fcfa2a97
Eric Denis Laura <ericdenislaura@gmail.com>
2023-04-30 16:26:53 -0300

    routes

---
+++ src/app/edit/[id]/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = ({ params }) => {
+  return (
+    <div>
+      editando
+      <h3>{params.id}</h3>
+    </div>
+  );
+};
+
+export default Page;
--- src/app/new/page.js
+++ src/app/new/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Page = () => {
-  return <div>Home Page</div>;
+  return <div>task form</div>;
 };
 
 export default Page;
